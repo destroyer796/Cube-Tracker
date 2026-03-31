993d1d7a0df3ca0b5656beb49c4be45f70953da3
destroyer796 <liam.newbill@gmail.com>
2026-03-31 01:02:44 -0400

    IT WORKS!

--- Arduino Code/Hermes_ArduinoCode/Hermes_ArduinoCode.ino
+++ Arduino Code/Hermes_ArduinoCode/Hermes_ArduinoCode.ino
@@ -5,7 +5,6 @@
 #include <MadgwickAHRS.h>
 
 #define ICM_ADDR 0x6A
-//#define OLED_ADDR 0x79
 
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
@@ -15,44 +14,39 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 Madgwick filter;
 
-unsigned long lastMicros = 0; //used for making sure loop runs at constant time
-const unsigned long intervalMicros = 8921; //112.1Hz
+unsigned long microsPerReading, microsPrevious;
+float accelScale, gyroScale;
 
-//structure for Quaternion
-//struct Quaternion {
-  //float w, x, y, z;
-//};
-
-//structure for 3D Vector
+// Structure for 3D Vector
 struct Vector3 {
   float x, y, z;
 };
 
 Vector3 gyroBias = {0, 0, 0};
 
-//structure for screen projection
+// Structure for screen projection
 struct Vector2 {
   int x, y;
 };
 
-//define 8 cube vertices, centered at origin
+// Define 8 cube vertices, centered at origin
 Vector3 cubeVertices[8] = {
   {-1, -1, -1}, {1, -1, -1}, {1, 1, -1}, {-1, 1, -1},
   {-1, -1, 1}, {1, -1, 1}, {1, 1, 1}, {-1, 1, 1}
 };
 
-//define edges as pairs of vertex indices
+// Define edges as pairs of vertex indices
 const uint8_t edges[12][2] = {
-  {0,1}, {1,2}, {2,3}, {3,0}, //draws back face
-  {4,5}, {5,6}, {6,7}, {7,4}, //draws front face
-  {0,4}, {1,5}, {2,6}, {3,7} //draws connecting lines back to front
+  {0,1}, {1,2}, {2,3}, {3,0}, // Draws back face
+  {4,5}, {5,6}, {6,7}, {7,4}, // Draws front face
+  {0,4}, {1,5}, {2,6}, {3,7} // Draws connecting lines back to front
 };
 
 struct Quaternion { float w, x, y, z; };
 Quaternion q = {1, 0, 0, 0}; //identity quaternion to start
 
-//converts roll/pitch/yaw in degrees to a unit quaternion
-//rotation order = Z(yaw) * Y(pitch) * X(roll) which matches Madgwick’s getters
+// Converts roll/pitch/yaw in degrees to a unit quaternion
+// Rotation order = Z(yaw) * Y(pitch) * X(roll) which matches Madgwick’s getters
 Quaternion eulerDegToQuat(float rollDeg, float pitchDeg, float yawDeg) {
   float r = 0.5f * rollDeg  * DEG_TO_RAD;
   float p = 0.5f * pitchDeg * DEG_TO_RAD;
@@ -78,18 +72,23 @@ void initICM() {
 
   Wire.beginTransmission(ICM_ADDR);
   Wire.write(0x03); // CTRL2 Accelerometer Settings
-  Wire.write(0b00010110); // +-4g, 112.1Hz
+  Wire.write(0b00010111); // +-4g, 56.05Hz
   Wire.endTransmission();
 
   Wire.beginTransmission(ICM_ADDR);
   Wire.write(0x04); // CTRL3 Gyroscope Settings
-  Wire.write(0b01010110); // +=512dps, 112.1Hz
+  Wire.write(0b01010111); // +=512dps, 56.05Hz
   Wire.endTransmission();
 
   Wire.beginTransmission(ICM_ADDR);
   Wire.write(0x08); // CTRL7 Enable Sensors and Configure Data Reads
   Wire.write(0b00000011); // aEN=1, gEN=1, enables accel/gyro
   Wire.endTransmission();
+
+  //Wire.beginTransmission(ICM_ADDR);
+  //Wire.write(0x06); // CTRL5 Sensor Data Processing Settings
+  //Wire.write(0b00110011); // Enable accel and gyro Low-Pass-Filter with 3.63Hz
+  //Wire.endTransmission();
 
   delay(200); // Wait for gyro to stabilize
 }
@@ -100,7 +99,7 @@ void calibrateGyro(int samples = 500) {
     float gx, gy, gz;
     readGyro(gx, gy, gz);
     sumX += gx; sumY += gy; sumZ += gz;
-    delayMicroseconds(intervalMicros);
+    delayMicroseconds(microsPerReading);
   }
   gyroBias.x = sumX / samples;
   gyroBias.y = sumY / samples;
@@ -122,9 +121,9 @@ void readAccel(float& ax, float& ay, float& az) {
   lo = Wire.read(); hi = Wire.read(); az_raw = (hi << 8) | lo;
 
   // +-4g
-  ax = ax_raw / 8192.0;  
-  ay = ay_raw / 8192.0;
-  az = az_raw / 8192.0;
+  ax = (ax_raw * 4) / 32768.0;
+  ay = (ay_raw * 4) / 32768.0;
+  az = (az_raw * 4) / 32768.0;
 }
 
 
@@ -143,10 +142,11 @@ void readGyro(float& gx, float& gy, float& gz) {
   lo = Wire.read(); hi = Wire.read(); gz_raw = (hi << 8) | lo;
 
   // +-512dps
-  gx = gx_raw / 64.0; // Converts the raw value to the degrees per second
-  gy = gy_raw / 64.0;
-  gz = gz_raw / 64.0;
+  gx = (gx_raw * 512) / 32768.0; // Convert to deg/sec
+  gy = (gy_raw * 512) / 32768.0;
+  gz = (gz_raw * 512) / 32768.0;
 }
+
 
 void setup() {
   Serial.begin(115200);
@@ -154,82 +154,66 @@ void setup() {
   Wire.setClock(400000);
   delay(500);
 
-// SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  microsPerReading = 1000000 / 56.05;
+  microsPrevious = micros();
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+    for(;;);
   }
 
   display.clearDisplay();
-  //display.drawPixel(10, 10, SSD1306_WHITE);
-  //display.display();
-  //delay(1000); // Pause for 2 seconds
-
-  //display.begin();
-  //display.clearDisplay();
-  //display.setTextColor(SSD1306_WHITE);
-  //display.setTextSize(1);
-  //display.setCursor(0, 0);
-  // delay(100);
-  // display.drawLine(0, 0, 20, 20, SSD1306_WHITE);
-  //delay(100000);
 
   initICM();
-
   calibrateGyro();
-
-  filter.begin(112.1); //112.1Hz
+  filter.begin(56.05);
+  
   Serial.print("Setup complete");
 }
 
 void loop() {
-  unsigned long now = micros();
-  if (micros() - lastMicros > intervalMicros){
-    Serial.println(micros() - lastMicros);
-    unsigned long now = micros();
-    float dt = (now - lastMicros) * 1e-6f;
-    lastMicros = now;
+  int aix, aiy, aiz;
+  int gix, giy, giz;
+  float ax, ay, az;
+  float gx, gy, gz;
+  float roll, pitch, heading;
+  unsigned long microsNow;
 
-    float ax, ay, az, gx, gy, gz;
+  microsNow = micros();
+  if (microsNow - microsPrevious >= microsPerReading) {
     readAccel(ax, ay, az);
     readGyro(gx, gy, gz);
 
-    //Serial.print("Accel (g): ");
-    //Serial.print(ax); Serial.print(", ");
-    //Serial.print(ay); Serial.print(", ");
-    //Serial.println(az);
-
-    //Serial.print("Gyro (dps): ");
-    //Serial.print(gx); Serial.print(", ");
-    //Serial.print(gy); Serial.print(", ");
-    //Serial.println(gz);
-
     filter.updateIMU(
-      radians(gx - gyroBias.x),
-      radians(gy - gyroBias.y),
-      radians(gz - gyroBias.z),
+      (gy - gyroBias.y),
+      (gx - gyroBias.x),
+      (gz - gyroBias.z),
       ax, ay, az
     );
 
-    //Serial.print("Roll: ");  Serial.print(roll);
-    //Serial.print(" Pitch: "); Serial.print(pitch);
-    //Serial.print(" Yaw: ");   Serial.println(yaw);
+    roll = filter.getRoll();
+    pitch = filter.getPitch();
+    heading = filter.getYaw();
 
-    float roll  = filter.getRoll();
-    float pitch = filter.getPitch();
-    float yaw   = filter.getYaw();
-    q = eulerDegToQuat(roll, pitch, yaw);
+    Serial.print("Orientation: ");
+    Serial.print(heading);
+    Serial.print(" ");
+    Serial.print(pitch);
+    Serial.print(" ");
+    Serial.println(roll);
+    
+    q = eulerDegToQuat(roll, pitch, heading);
 
     static int frameSkip = 0;
-
-    if (++frameSkip >= 10) { // ~11 Hz display
+    if (++frameSkip >= 5) { // ~22 Hz display
       frameSkip = 0;
-      //unsigned long t0 = micros();
-      display.clearDisplay();
-      drawCube(q);
-      display.display();
-      //Serial.println(micros() - t0);
+        display.clearDisplay();
+        drawCube(q);
+        display.display();
     }
+    // Increment previous time, so we keep proper pace
+    microsPrevious = microsPrevious + microsPerReading;
   }
 }
 
@@ -243,29 +227,29 @@ Quaternion qMult(const Quaternion & a, const Quaternion & b) {
   };
 }
 
-//flips the sign of the imaginary parts, inverse
+// Flips the sign of the imaginary parts, inverse
 Quaternion qConj(const Quaternion& q){
   return {q.w, -q.x, -q.y, -q.z};
 }
 
-//rotate a 3D Vector using a quaternion
+// Rotate a 3D Vector using a quaternion
 Vector3 rotateByQuaternion(const Vector3& v, const Quaternion& q) {
-  Quaternion p ={0, v.x, v.y, v.z}; //Represent the vector as a quaternion (pure quaternion, w=0)
-  Quaternion qInv = qConj(q); //Invert the quaternion rotation
-  Quaternion result = qMult(qMult(q,p), qInv); //Rotate the vector using quaternion sandwitch formula, rotatedVector = q * p * q^-1
-  return {result.x, result.y, result.z}; //Return the rotated vector part, ignore scalar w
+  Quaternion p ={0, v.x, v.y, v.z}; // Represent the vector as a quaternion (pure quaternion, w=0)
+  Quaternion qInv = qConj(q); // Invert the quaternion rotation
+  Quaternion result = qMult(qMult(q,p), qInv); // Rotate the vector using quaternion sandwitch formula, rotatedVector = q * p * q^-1
+  return {result.x, result.y, result.z}; // Return the rotated vector part, ignore scalar w
 }
 
-//project a 3D point to 2D coordinates, orthographic
+// Project a 3D point to 2D coordinates, orthographic
 Vector2 projectTo2D(const Vector3& v) {
-  float scale = 20.0; //Adjust for the cube size
+  float scale = 20.0; // Adjust for the cube size
   return {
     (int)(v.x * scale) + SCREEN_WIDTH / 2,
     (int)(-v.y * scale) + SCREEN_HEIGHT / 2
   };
 }
 
-//draw the cube from the quaternion rotation
+// Draw the cube from the quaternion rotation
 void drawCube(const Quaternion& q){
   Vector2 projected[8];
 
